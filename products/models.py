a8fa040c28addaca640f88a6d51c8e54502b76c9
rizkysampratama <rizkysampratama@gmail.com>
2019-08-06 17:42:55 +0700

    Models Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Product Class
+class Product(models.Model):
+
+    def __str__(self):
+        return self.title
+    
+    # title
+    title = models.CharField(max_length=255)
+    
+    # url
+    url = models.TextField()
+    
+    # pub_date
+    pub_date = models.DateTimeField()
+    
+    # votes_total
+    votes_total = models.IntegerField(default=1)
+
+    # images
+    image = models.ImageField(upload_to='images/')
+    
+    # icon
+    icon = models.ImageField(upload_to='images/')    
+    
+    # Body
+    body = models.TextField()
+    
+    # pub_date_pretty
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    
+    def summary(self):
+        return self.body[:100]
+
+    # hunter
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    
